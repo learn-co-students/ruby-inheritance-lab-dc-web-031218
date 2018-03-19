51a634444f3e517643bd077ea5d79190525e9a49
SaturdayAM <jwl62@georgetown.edu>
2018-03-19 09:51:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
+require_relative './user.rb'
 
+class Student < User
+	def initialize(first_name = nil, last_name = nil)
+		@first_name, @last_name = first_name, last_name
+		@knowledge = []
+	end
+
+	def learn(str)
+		@knowledge << str
+	end
+
+	def knowledge
+		@knowledge
+	end
 end
---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+	EXAMPLE = "example"
+
+	attr_accessor :first_name, :last_name
+	def initialize(first_name = nil, last_name = nil)
+		@first_name, @last_name = first_name, last_name
+	end
+
+
+	def EXAMPLE
+		EXAMPLE
+	end
+
+
+
+end
