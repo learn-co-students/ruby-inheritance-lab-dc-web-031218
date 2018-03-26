584befd53a084c86a565cc04a4b0feadd7aa9031
deepgpatel <deepgpatel93@gmail.com>
2018-03-26 09:46:20 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,13 @@
 class Student < User
-attr_accessor :first_name, :last_name
+attr_accessor :first_name, :last_name, :knowledge
 
 
   def initialize
-    @first_name = first_name
-    @last_name = last_name
     @knowledge = []
   end
 
   def learn(information)
     @knowledge << information
-  end
-
-  def knowledge
-    @knowledge
   end
 
 
